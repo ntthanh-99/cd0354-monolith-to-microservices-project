19b6f3de9ea222e47a145bd6edcf3259fa7c3873
ThanhNT <63948294+ntthanh-99@users.noreply.github.com>
2023-06-04 21:07:04 +0700

    Update environment.prod.ts

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost: "http://ab2600b7920e1443e8c3827ca215038dab2600b7920e1443e8c3827ca215038d:8080/api/v0",
 };
 
 /*
